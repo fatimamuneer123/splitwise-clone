925298bd8d0386a380f5c5355b19f2d97f0bcdb1
Fatima <fatima.muneer.1213@gmail.com>
2024-06-03 19:46:56 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Layout from './layout';
+import Login from './Login';
+import SignUp from './SignUp';
+import AddExpense from './AddExpense';
+import Expenses from './Expenses';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/add-expense" element={<AddExpense />} />
+          <Route path="/expenses" element={<Expenses />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
-}
+};
 
 export default App;
+
+
+// import React, { useState, useEffect } from 'react';
+// import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+// import Login from './Login';
+// import SignUp from './SignUp';
+// import AddExpense from './AddExpense';
+// import Expenses from './Expenses';
+// import { auth, onAuthStateChanged } from './firebase';
+
+// const ProtectedRoute = ({ element, ...rest }) => {
+//   const [user, setUser] = useState(null);
+
+//   useEffect(() => {
+//     const unsubscribe = onAuthStateChanged(auth, (user) => {
+//       setUser(user);
+//     });
+
+//     return () => unsubscribe();
+//   }, []);
+
+//   return user ? (
+//     <Route {...rest} element={element} />
+//   ) : (
+//     <Navigate to="/" replace />
+//   );
+// };
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <div>
+//         <Routes>
+//           <Route path="/" element={<Login />} />
+//           <Route path="/signup" element={<SignUp />} />
+//           <ProtectedRoute path="/add-expense" element={<AddExpense />} />
+//           <ProtectedRoute path="/expenses" element={<Expenses />} />
+//         </Routes>
+//       </div>
+//     </Router>
+//   );
+// };
+
+// export default App;
